dba93207fc17cd59690108dc75177138fc2c0cb8
Dorian Bucur <sirdorius@gmail.com>
2025-06-17 02:46:43 +0200

    Added enemy health progression

--- src/Enemies.cs
+++ src/Enemies.cs
@@ -6,7 +6,7 @@ using static System.Linq.Enumerable;
 namespace flecs_test;
 
 record struct Enemy;
-record struct EnemySpawner(bool Placeholder);
+record struct EnemySpawner(uint Level = 1);
 
 class EnemiesModule : IFlecsModule
 {
@@ -14,18 +14,22 @@ class EnemiesModule : IFlecsModule
 	{
 		foreach (int i in Range(1, 5))
 		{
-			SpawnEnemy(ref world, new Vector2(100 * i, 20));
+			SpawnEnemy(ref world, new Vector2(100 * i, 20), 1);
 		}
-		world.Entity().Set(new EnemySpawner());
+		world.Entity().Set(new EnemySpawner(1));
 
 		world.System<Transform, PhysicsBody>()
 			.With<Enemy>()
 			.Kind(Ecs.PreUpdate)
 			.Iter(FollowPlayer);
 
-		var tickSource = world.Timer().Interval(500f);
 		world.System<EnemySpawner>()
-			.TickSource(tickSource)
+			.TickSource(world.Timer().Interval(5000f))
+			.Kind(Ecs.PreUpdate)
+			.Each(IncrementLevel);
+
+		world.System<EnemySpawner>()
+			.TickSource(world.Timer().Interval(500f))
 			.Kind(Ecs.PreUpdate)
 			.Immediate()
 			.Iter(SpawnEnemies);
@@ -34,6 +38,13 @@ class EnemiesModule : IFlecsModule
 			.With<Enemy>()
 			.Event(Ecs.OnRemove)
 			.Each(HandleDeath);
+
+	}
+	static void IncrementLevel(ref EnemySpawner spawner)
+	{
+		if (spawner.Level >= 5) return;
+		spawner.Level += 1;
+		Console.WriteLine($"Enemy levels: {spawner.Level}");
 	}
 
 	static void FollowPlayer(Iter it, Field<Transform> transform, Field<PhysicsBody> body)
@@ -51,7 +62,7 @@ class EnemiesModule : IFlecsModule
 		}
 	}
 
-	static void SpawnEnemies(Iter it, Field<EnemySpawner> t0)
+	static void SpawnEnemies(Iter it, Field<EnemySpawner> spawner)
 	{
 		var world = it.World();
 		var playerQ = world.QueryBuilder().With<Player>().Build();
@@ -62,22 +73,29 @@ class EnemiesModule : IFlecsModule
 		{
 			var angle = Random.Shared.NextSingle() * MathF.PI * 2;
 			var pos = Vector2.Rotate(Vector2.UnitX, angle) * RADIUS + playerTransform.Pos;
-			SpawnEnemy(ref world, pos);
+			SpawnEnemy(ref world, pos, spawner[i].Level);
 		}
 	}
 
-	static void SpawnEnemy(ref World world, Vector2 pos)
+	static void SpawnEnemy(ref World world, Vector2 pos, uint level)
 	{
 		var enemy = world.Entity()
 			.Add<Enemy>()
 			.Set(new Transform(pos, Vector2.One, 0))
 			.Set(new PhysicsBody(new Vector2(1, 1), Vector2.Zero))
 			.Set(new Collider(17))
-			.Set(new Health(2))
+			.Set(new Health((int)level))
 			.Observe<CollisionEvent>(HandleEnemyHit);
+		var sprite = level switch
+		{
+			1 => "sprites/alienBeige_walk1",
+			2 => "sprites/alienYellow_walk1",
+			3 => "sprites/alienBlue_walk1",
+			_ => "sprites/alienPink_walk1",
+		};
 		world.Entity()
 			.Set(new Transform(new Vector2(0, 15), new Vector2(0.5f, 0.5f), 0))
-			.Set(new Sprite("sprites/alienPink_walk1"))
+			.Set(new Sprite(sprite))
 			.ChildOf(enemy);
 	}
 
--- todo.md
+++ todo.md
@@ -1,18 +1,23 @@
 ## priority 1
+- nullables
+- mouse movement
 - camera movement
 - y sorting
-- mouse movement
 - penetrating bullets (max number of penetrations)
-- enemy health progression
 
 ## priority 2
 - add acceleration to movement
 - more weapon types
 - spatial hashing
 - enemies use flow field
-- particles
-- ui
+- particle effects
+- animations sprite changes
+- animations tweens
 
 ## priority 3
+- ui
+- sound
 - better GlobalTransform using change detection on Transform https://www.flecs.dev/flecs/md_docs_2Queries.html#change-detection
 - roguelite powerups
+- menus
+- flocking behaviors
